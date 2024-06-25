8f97d7ecd0a27e3dbb7bce0f0a94fd5a555be397
Hong, SeokHyeon <tyuiop929@gmail.com>
2024-06-25 22:27:50 +0900

    feat: 초기설정

---
+++ src/test/java/com/example/nangmanmemo/NangmanmemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nangmanmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NangmanmemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
