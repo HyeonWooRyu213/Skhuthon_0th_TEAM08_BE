4238952c8f47f5d2086bb038889d34228c48b02a
HyeonWooRyu213 <fbgusdn05@gmail.com>
2024-06-29 16:13:57 +0900

    수정

--- src/main/java/com/example/nangmanmemo/comment/domain/repository/CommentRepository.java
+++ src/main/java/com/example/nangmanmemo/comment/domain/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.example.nangmanmemo.comment.domain.repository;
 
 import com.example.nangmanmemo.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
